9dd55d796393ca7ffac02c56cd10961e08ee52b4
Ian Logan <itlogan9@gmail.com>
2020-11-25 13:21:19 -0600

    Add table creation queries

--- sql_queries.py
+++ sql_queries.py
@@ -1,26 +1,77 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+    --sql
+    CREATE TABLE IF NOT EXISTS songplays(
+        songplay_id SERIAL PRIMARY KEY,
+        start_time TIMESTAMP,
+        user_id INTEGER,
+        level INTEGER,
+        song_id VARCHAR,
+        artist_id VARCHAR, 
+        session_id INTEGER,
+        location VARCHAR,
+        user_agent VARCHAR,
+        FOREIGN KEY(start_time) REFERENCES time(start_time),
+        FOREIGN KEY(user_id) REFERENCES users(user_id),
+        FOREIGN KEY(song_id) REFERENCES songs(song_id),
+        FOREIGN KEY(artist_id) REFERENCES artists(artist_id)
+    );
 """)
 
 user_table_create = ("""
+    --sql
+    CREATE TABLE IF NOT EXISTS users(
+        user_id SERIAL PRIMARY KEY,
+        first_name VARCHAR,
+        last_name VARCHAR,
+        gender CHAR(1),
+        level VARCHAR
+    );
 """)
 
 song_table_create = ("""
+    --sql
+    CREATE TABLE IF NOT EXISTS songs(
+        song_id VARCHAR PRIMARY KEY,
+        title VARCHAR,
+        artist_id VARCHAR,
+        year INTEGER,
+        duration REAL,
+        FOREIGN KEY(artist_id) REFERENCES artists(artist_id)
+    );
 """)
 
 artist_table_create = ("""
+    --sql
+    CREATE TABLE IF NOT EXISTS artists(
+        artist_id VARCHAR PRIMARY KEY,
+        name VARCHAR,
+        location VARCHAR,
+        latitude DOUBLE PRECISION,
+        longitude DOUBLE PRECISION
+    );
 """)
 
 time_table_create = ("""
+    --sql
+    CREATE TABLE IF NOT EXISTS time(
+        start_time TIMESTAMP PRIMARY KEY,
+        hour INTEGER, 
+        day INTEGER,
+        week INTEGER,
+        month INTEGER,
+        year INTEGER,
+        weekday VARCHAR(9)
+    );
 """)
 
 # INSERT RECORDS
@@ -48,5 +99,5 @@ song_select = ("""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
